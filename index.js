ec2b3fca8201f9a2d7274b15e945d7b975a44ea6
Alan Silva <silzen.ent@gmail.com>
2023-11-30 13:09:37 -0600

    Completed Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return {
+        ...object,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value; 
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newObject = { ...object };
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
